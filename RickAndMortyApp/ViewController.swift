fa36e3051e5f920554b8a804ddd34bbfaf5af633
Mikheil Muchaidze <mmuchaidze@tbcbank.com.ge>
2024-01-12 17:51:42 +0400

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by mmuchaidze on 12.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
