cfbdcb7009e90d7cb71814b3cd8671a14bcd2ae6
Mikheil Muchaidze <mmuchaidze@tbcbank.com.ge>
2024-01-20 13:31:08 +0400

    Video 15, Added skeleton of compositional layout collection view, visually have three sections and each is configured as needed, no actual datasource yet, configuring of sections moved to viewmodel

--- RickAndMortyApp/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMortyApp/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by mmuchaidze on 15.01.24.
 //
 
-import Foundation
+import UIKit
 
 final class RMCharacterDetailViewViewModel {
     // MARK: - Private Properties
@@ -31,5 +31,81 @@ final class RMCharacterDetailViewViewModel {
     
     init(character: RMCharacter) {
         self.character = character
+    }
+    
+    // MARK: - Public Properties
+    
+    public func createPhotoSectionLayout()  -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 0,
+            bottom: 10,
+            trailing: 0
+        )
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(0.5)
+            ),
+            subitems: [item]
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    public func createInfoSectionLayout()  -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.5),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: 5,
+            leading: 0,
+            bottom: 5,
+            trailing: 0
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(150)
+            ),
+            subitems: [item, item]
+        )
+        group.interItemSpacing = .fixed(10)
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    public func createEpisodeSectionLayout()  -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: 10,
+            leading: 0,
+            bottom: 10,
+            trailing: 8
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.8),
+                heightDimension: .absolute(150)
+            ),
+            subitems: [item]
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        return section
     }
 }
