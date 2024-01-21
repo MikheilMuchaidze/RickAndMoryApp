085ccc3fc8a6cfac8488d851c56d22de7cc1515c
Mikheil Muchaidze <mmuchaidze@tbcbank.com.ge>
2024-01-21 23:28:15 +0400

    Video 18, Added an labels and image views to he character details views info collection view cell, added constraints, visibility and skeloton is setuped for testing, needs actual data feeding, prepare for reuse functionality is commented for now

--- RickAndMortyApp/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
+++ RickAndMortyApp/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
@@ -13,12 +13,47 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     
     static let cellIdentifier = "RMCharacterInfoCollectionViewCell"
     
+    // MARK: - Private Properties
+    
+    private let titleContainerView: UIImageView = {
+        let titleContainerView = UIImageView()
+        titleContainerView.translatesAutoresizingMaskIntoConstraints = false
+        titleContainerView.backgroundColor = .secondarySystemBackground
+        titleContainerView.layer.cornerRadius = 8
+        return titleContainerView
+    }()
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Earth"
+        label.font = .systemFont(ofSize: 22, weight: .light)
+        return label
+    }()
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Location"
+        titleLabel.textAlignment = .center
+        titleLabel.font = .systemFont(ofSize: 20, weight: .medium)
+        return titleLabel
+    }()
+    private let iconImageView: UIImageView = {
+        let iconImageView = UIImageView()
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.image = UIImage(systemName: "globe.americas")
+        iconImageView.contentMode = .scaleAspectFit
+        return iconImageView
+    }()
+    
     // MARK: - Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        contentView.addSubViews(titleContainerView, valueLabel, iconImageView)
+        titleContainerView.addSubview(titleLabel)
         addConstraints()
     }
     
@@ -28,12 +63,35 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+//        valueLabel.text = nil
+//        titleLabel.text = nil
+//        iconImageView.image = nil
     }
     
     // MARK: - Private Methods
     
     private func addConstraints() {
-        
+        NSLayoutConstraint.activate([
+            titleContainerView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            titleContainerView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33),
+            
+            titleLabel.leftAnchor.constraint(equalTo: titleContainerView.leftAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: titleContainerView.rightAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: titleContainerView.bottomAnchor),
+            titleLabel.topAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            
+            iconImageView.heightAnchor.constraint(equalToConstant: 30),
+            iconImageView.widthAnchor.constraint(equalToConstant: 30),
+            iconImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35),
+            iconImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            
+            valueLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 10),
+            valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            valueLabel.heightAnchor.constraint(equalToConstant: 30),
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 36),
+        ])
     }
     
     // MARK: - Public Methods
