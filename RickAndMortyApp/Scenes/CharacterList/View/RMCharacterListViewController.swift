0c2b8d77d1ee4583401a17893056ebff184020f6
Mikheil Muchaidze <mmuchaidze@tbcbank.com.ge>
2024-01-30 14:37:10 +0400

    Refactored character list scene to some type of mvvm

---
+++ RickAndMortyApp/Scenes/CharacterList/View/RMCharacterListViewController.swift
@@ -0,0 +1,204 @@
+//
+//  RMCharacterListViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Mikheil Muchaidze on 14.01.24.
+//
+
+import UIKit
+
+/// View that handles showing  list of characters, loader, etc.
+final class RMCharacterViewController: UIViewController {
+    // MARK: - Private Properties
+    
+    private let viewModel = RMCharacterListViewControllerViewModel()
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(
+            top: 0,
+            left: 10,
+            bottom: 10,
+            right: 10
+        )
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(
+            RMCharacterCollectionViewCell.self,
+            forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier
+        )
+        collectionView.register(
+            RMFooterLoadingCollectionReusableView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
+            withReuseIdentifier: RMFooterLoadingCollectionReusableView.cellIdentifier
+        )
+        return collectionView
+    }()
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Characters"
+        setupView()
+        viewModel.fetchCharacters()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupView() {
+        view.addSubViews(collectionView, spinner)
+        registerViewModelListener()
+        addConstraints()
+        setupCollectionView()
+        spinner.startAnimating()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    private func registerViewModelListener() {
+        viewModel.registerForData { [weak self] updateModel in
+            guard let self else { return }
+            switch updateModel {
+            case .initialLoad:
+                didLoadInitialCharacters()
+            case .paginationLoad(let index):
+                didLoadMoreCharacters(with: index)
+            }
+        }
+    }
+    
+    private func navigateToCharacterDetailWith(character: RMCharacter) {
+        let viewModel = RMCharacterDetailViewViewModel(character: character)
+        let characterDetailVC = RMCharacterDetailViewController(viewModel: viewModel)
+        navigationController?.pushViewController(characterDetailVC, animated: true)
+    }
+    
+    private func didLoadInitialCharacters() {
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData() // Initial fetch
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
+    }
+    
+    private func didLoadMoreCharacters(with newIndexPaths: [IndexPath]) {
+        collectionView.insertItems(at: newIndexPaths)
+    }
+}
+
+// MARK: - UICollectionViewDataSource & general cell deqeue
+
+extension RMCharacterViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.getCellCount()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier,
+            for: indexPath
+        ) as? RMCharacterCollectionViewCell else {
+            fatalError("Unsupported cell")
+        }
+        cell.configure(viewModel.getCellViewModelForConfiguration(with: indexPath.row))
+        return cell
+    }
+}
+
+// MARK: - CollectionView footer cell deqeue
+
+extension RMCharacterViewController {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionFooter, let footer = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: RMFooterLoadingCollectionReusableView.cellIdentifier,
+            for: indexPath
+        ) as? RMFooterLoadingCollectionReusableView else {
+            fatalError("Unsupported")
+        }
+        
+        footer.startAnimating()
+        return footer
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        guard viewModel.getShouldShowLoadMoreIndicatorValue() else { return .zero }
+        
+        return CGSizeMake(collectionView.frame.width, 100)
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
+
+extension RMCharacterViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width-30)/2
+        return CGSize(
+            width: width,
+            height: width * 1.5
+        )
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension RMCharacterViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard viewModel.shouldShowLoadMoreIndicator,
+              !viewModel.isLoadingMore,
+              let nextUrlString = viewModel.getApiInfoNextString(),
+              let url = URL(string: nextUrlString)
+        else { return }
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { [weak self] t in
+            guard let self else { return }
+            
+            let offset = scrollView.contentOffset.y
+            let totalContentHeight = scrollView.contentSize.height
+            let totalScrollViewHeight = scrollView.frame.size.height
+            
+            if offset >= (totalContentHeight - totalScrollViewHeight - 120) {
+                viewModel.fetchAdditionalCharacters(url: url)
+            }
+            t.invalidate()
+        }
+    }
+}
+
+// MARK: - CollectionView didSelect
+
+extension RMCharacterViewController {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        navigateToCharacterDetailWith(character: viewModel.getSelectedCharacter(with: indexPath.row))
+    }
+}
---
+++ RickAndMortyApp/Scenes/CharacterList/ViewModel/RMCharacterListViewControllerViewModel.swift
@@ -0,0 +1,153 @@
+//
+//  RMCharacterListViewControllerViewModel.swift
+//  RickAndMortyApp
+//
+//  Created by Mikheil Muchaidze on 14.01.24.
+//
+
+import Foundation
+
+protocol RMCharacterListViewControllerViewModelProtocol: AnyObject {
+    func getCellCount() -> Int
+    func getCellViewModelForConfiguration(with index: Int) -> RMCharacterCollectionViewCellViewModel
+    func getSelectedCharacter(with index: Int) -> RMCharacter
+    func getApiInfoNextString() -> String?
+}
+
+public enum UpdateModel {
+    case initialLoad
+    case paginationLoad([IndexPath])
+}
+
+/// View Model to handle character list view logic
+final class RMCharacterListViewControllerViewModel: RMCharacterListViewControllerViewModelProtocol {
+    // MARK: - Private Properties
+    
+    private var characters: [RMCharacter] = [] {
+        didSet {
+            characters.forEach {
+                let viewModel = RMCharacterCollectionViewCellViewModel(
+                    characterName: $0.name,
+                    characterStatus: $0.status,
+                    characterImageUrl: URL(string: $0.image)
+                )
+                if !cellViewModels.contains(viewModel) {
+                    cellViewModels.append(viewModel)
+                }
+            }
+        }
+    }
+    private var cellViewModels: [RMCharacterCollectionViewCellViewModel] = []
+    private var apiInfo: RMGetAllCharacterResponse.RMGetAllCharacterResponseInfo? = nil
+    private(set) var isLoadingMore = false
+    private var updateModel: ((UpdateModel) -> Void)?
+    
+    // MARK: - Public Properties
+    
+    var shouldShowLoadMoreIndicator: Bool {
+        apiInfo?.next != nil
+    }
+    
+    // MARK: - Private Methods
+    
+    /// Getting an index for a insertion in collection view
+    /// - Returns: Array of indexpaths to insert in collection
+    private func getIndexForInsertion(results: [RMCharacter]) -> [IndexPath] {
+        var indexPathsToAdd: [IndexPath] = []
+        
+        let originalCount = characters.count
+        let newCount = results.count
+        let total = originalCount + newCount
+        let startingIndex = total - newCount
+        indexPathsToAdd = Array(startingIndex..<(startingIndex + newCount)).compactMap { IndexPath(row: $0, section: 0) }
+        
+        return indexPathsToAdd
+    }
+    
+    // MARK: - Public Methods
+    
+    public func registerForData(_ block: @escaping (UpdateModel) -> Void) {
+        updateModel = block
+    }
+    
+    /// Fetch initail set of characters (20)
+    public func fetchCharacters() {
+        RMService.shared.execute(
+            .listCharactersRequest,
+            expecting: RMGetAllCharacterResponse.self
+        ) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let responseModel):
+                let info = responseModel.info
+                let results = responseModel.results
+                characters = results
+                apiInfo = info
+                DispatchQueue.main.async {
+                    self.updateModel?(.initialLoad)
+                }
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+    
+    /// Paginate if additional characters are needed (paginatipn)
+    public func fetchAdditionalCharacters(url: URL) {
+        guard !isLoadingMore else {
+            return
+        }
+        isLoadingMore = true
+        guard let request = RMRequest(url: url) else {
+            isLoadingMore = false
+            print("Failed to create request")
+            return
+        }
+        
+        RMService.shared.execute(
+            request,
+            expecting: RMGetAllCharacterResponse.self
+        ) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let responseModel):
+                let info = responseModel.info
+                let moreResults = responseModel.results
+                apiInfo = info
+                let getIndexForInsertion = self.getIndexForInsertion(results: moreResults)
+                characters.append(contentsOf: moreResults)
+                DispatchQueue.main.async {
+                    self.updateModel?(.paginationLoad(getIndexForInsertion))
+                    self.isLoadingMore = false
+                }
+            case .failure(let error):
+                print(String(describing: error))
+                self.isLoadingMore = false
+            }
+        }
+    }
+    
+    public func getSelectedCharacter(with index: Int) -> RMCharacter {
+        characters[index]
+    }
+    
+    public func getCellViewModelForConfiguration(with index: Int) -> RMCharacterCollectionViewCellViewModel {
+        cellViewModels[index]
+    }
+    
+    public func getCellCount() -> Int {
+        cellViewModels.count
+    }
+    
+    public func getShouldShowLoadMoreIndicatorValue() -> Bool {
+        shouldShowLoadMoreIndicator
+    }
+    
+    public func getisLoadingMoreValue() -> Bool {
+        isLoadingMore
+    }
+    
+    public func getApiInfoNextString() -> String? {
+        apiInfo?.next
+    }
+}
