47781c4aa578743b42ae1e03afd5a7dd7f5bcb13
Mikheil Muchaidze <mmuchaidze@tbcbank.com.ge>
2024-02-04 01:09:48 +0400

    Video 26, setup for episode detail view, preparing viewModel

--- RickAndMortyApp/Scenes/EpisodeDetail/View/RMEpisodeDetailViewController.swift
+++ RickAndMortyApp/Scenes/EpisodeDetail/View/RMEpisodeDetailViewController.swift
@@ -6,18 +6,29 @@
 //
 
 import UIKit
+import Combine
 
 /// View Controller to show details about single episode
 final class RMEpisodeDetailViewController: UIViewController {
     // MARK: - Private Properties
     
     private let viewModel: RMEpisodeDetailViewViewModel
+    private var collectionView: UICollectionView?
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    private var cancellables: Set<AnyCancellable> = []
     
     // MARK: - Init
     
     init(url: URL?) {
         self.viewModel = RMEpisodeDetailViewViewModel(enpointUrl: url)
         super.init(nibName: nil, bundle: nil)
+        let collectionVIew = createCollectionView()
+        self.collectionView = collectionVIew
     }
     
     required init?(coder: NSCoder) {
@@ -30,14 +41,33 @@ final class RMEpisodeDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Episode"
+        viewModel.fetchEpisodeData()
         setupView()
     }
     
     // MARK: - Private Methods
     
     private func setupView() {
+        guard let collectionView else { return }
+        view.addSubViews(collectionView, spinner)
+        spinner.startAnimating()
+        registerViewModelListener()
         setupBarButton()
         addConstraints()
+        setupCollectionView()
+    }
+    
+    private func registerViewModelListener() {
+        viewModel.$dataTuple
+            .dropFirst()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] dataSource in
+                guard let self else { return }
+                view.backgroundColor = .red
+                print("update")
+                spinner.stopAnimating()
+            }
+            .store(in: &cancellables)
     }
     
     private func setupBarButton() {
@@ -49,6 +79,56 @@ final class RMEpisodeDetailViewController: UIViewController {
     }
     
     private func addConstraints() {
+        guard let collectionView else { return }
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setupCollectionView() {
+//        collectionView?.delegate = self
+//        collectionView?.dataSource = self
+    }
+    
+    private func createCollectionView() -> UICollectionView {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+            self.createSection(for: sectionIndex)
+        }
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+         
+        return collectionView
+    }
+    
+    private func createSection(for sectionIndex: Int) -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: .init(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+        )
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(100)
+            ),
+            subitems: [item]
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    private func configure() {
         
     }
     
--- RickAndMortyApp/Scenes/EpisodeDetail/ViewModel/RMEpisodeDetailViewViewModel.swift
+++ RickAndMortyApp/Scenes/EpisodeDetail/ViewModel/RMEpisodeDetailViewViewModel.swift
@@ -12,16 +12,20 @@ final class RMEpisodeDetailViewViewModel {
     
     private let enpointUrl: URL?
     
+    // MARK: - Published Properties
+    
+    @Published var dataTuple: (RMEpisode, [RMCharacter])?
+    
     // MARK: - Init
     
     init(enpointUrl: URL?) {
         self.enpointUrl = enpointUrl
-        fetchEpisodeData()
     }
     
-    // MARK: - Private Methods
+    // MARK: - Public Methods
     
-    private func fetchEpisodeData() {
+    /// Fetch backing episode model
+    public func fetchEpisodeData() {
         guard
             let url = enpointUrl,
             let requet = RMRequest(url: url)
@@ -34,10 +38,38 @@ final class RMEpisodeDetailViewViewModel {
             guard let self else { return }
             switch result {
             case .success(let responseModel):
-                print(String(describing: responseModel))
+                fetchRelatedCharacters(episode: responseModel)
             case .failure(let error):
                 print(String(describing: error))
             }
         }
+    }
+    
+    // MARK: - Private Methods
+    
+    private func fetchRelatedCharacters(episode: RMEpisode) {
+        let requests = episode.characters
+            .compactMap { URL(string: $0) }
+            .compactMap { RMRequest(url: $0) }
+        var characters: [RMCharacter] = []
+        
+        requests.forEach {
+            RMService.shared.execute(
+                $0,
+                expecting: RMCharacter.self
+            ) { result in
+                switch result {
+                case .success(let character):
+                    characters.append(character)
+                case .failure:
+                    break
+                }
+            }
+        }
+        
+        dataTuple = (
+            episode,
+            characters
+        )
     }
 }
